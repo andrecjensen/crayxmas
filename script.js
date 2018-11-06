99d7729a4ec485130b2fd8a0e8364b490797300a
André Jensen <Jensens@marie.kea.local>
2018-11-06 13:57:53 +0100

    En del tilføjelser

--- script.js
+++ script.js
@@ -14,8 +14,16 @@ function showStart() {
     document.querySelector("#start").classList.add("show");
     document.querySelector("#start .play").classList.add("pulse");
     document.querySelector("#start .play").addEventListener("click", hideStart);
+
+    document.querySelector("#ui-settings").addEventListener("click", showSettings);
+    document.querySelector("#setting_close").addEventListener("click", showSettings);
 }
 
+function showSettings() {
+    console.log("showSettings");
+    document.querySelector("#settings").classList.toggle("hide");
+
+}
 
 function hideStart() {
     document.querySelector("#startsound").play();
@@ -35,7 +43,7 @@ function startGame() {
     document.querySelector("#mymusic").play();
 
     // dette anfører at der gives 30 sekunder til at udføre spillet og at når tiden er løbet tør, vises gameover skærmen
-    setTimeout(gameOver, 30000);
+    setTimeout(gameOver, 300000);
 
     // Alle farvekridte gøres klikbare og føres til clickCrayon funktionen
     document.querySelector("#pop-farvekridt1-upleft").addEventListener("click", clickCrayon);
@@ -75,20 +83,24 @@ function clickCrayon() {
     //Dette fortælle console log at der gives 1+ til counter(tælleren)
     console.log("click Crayon - 1+ til tælleren og forsvinde-lyd spilles");
 
-    document.querySelector("#crayonclick").play();
+    if (counter < 1) {
+        document.querySelector("#crayonclick").play();
 
-    console.log(this);
-    this.classList.add("pause");
-    console.log(this);
-    this.classList.add("fadeoff");
+        console.log(this);
+        this.classList.add("pause");
+        console.log(this);
+        this.classList.add("fadeoff");
 
-    //    document.querySelector("#pop-farvekridt1-upleft").classList.add("pause");
-    //    document.querySelector("#pop-farvekridt1-upleft").classList.add("fadeoff");
+        //    document.querySelector("#pop-farvekridt1-upleft").classList.add("pause");
+        //    document.querySelector("#pop-farvekridt1-upleft").classList.add("fadeoff");
 
-    // Dette siger at antallet af tallet i "counter" skal plusses med 1.
-    counter++;
-    //Dette ændrer indholdet af "counter" div'et til det aktuelle tal
-    document.querySelector("#counter").innerHTML = counter;
+        // Dette siger at antallet af tallet i "counter" skal plusses med 1.
+        counter++;
+        //Dette ændrer indholdet af "counter" div'et til det aktuelle tal
+        document.querySelector("#counter").innerHTML = counter;
+    } else {
+        levelComplete();
+    }
 
 }
 
@@ -98,35 +110,42 @@ function clickGood() {
 
     console.log("click good");
 
-    document.querySelector("#goodclick").play();
+    if (life > 0) {
+        document.querySelector("#goodclick").play();
 
-    // fortæller at nissehue selectoren betyder #hue + antallet af life
-    let nissehue = "#hue" + life;
+        // fortæller at nissehue selectoren betyder #hue + antallet af life
+        let nissehue = "#hue" + life;
 
-    // dette vælger "nissehue" som har definiret hue + antallet af liv og giver den classen .hide.
-    document.querySelector(nissehue).classList.add("hide");
+        // dette vælger "nissehue" som har definiret hue + antallet af liv og giver den classen .hide.
+        document.querySelector(nissehue).classList.add("hide");
 
-    // Fortæller console log om antallet
-    console.log("#hue" + life);
+        // Fortæller console log om antallet
+        console.log("#hue" + life);
 
-    // dette trækker et tal ud af antallet af life
-    life--;
-    console.log(life);
+        // dette trækker et tal ud af antallet af life
+        life--;
+        console.log(life);
 
-    // her prøver jeg at sige, at hvis life er lig med 0 (nul liv), så skal game over skærmen vises
-
-
+        // her prøver jeg at sige, at hvis life er lig med 0 (nul liv), så skal game over skærmen vises
+    } else {
+        gameOver();
+    }
 }
 
 function gameOver() {
     document.querySelector("#game").classList.add("hide");
     document.querySelector("#gameover").classList.remove("hide");
 
-    document.querySelector("#replay").addEventListener("click", startOver);
+    document.querySelector(".replay").addEventListener("click", startOver);
+}
+
+function levelComplete() {
+    document.querySelector("#game").classList.add("hide");
+    document.querySelector("#levelcomplete").classList.remove("hide");
+
+    document.querySelector(".replay").addEventListener("click", startOver);
 }
 
 function startOver() {
-    document.querySelector("#gameover").classList.add("hide");
-
-    startGame();
+    window.location.reload(false);
 }
